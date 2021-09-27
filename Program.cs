0b78beb357cc7666def57f793d041e3f1c98b27d
Omar <excalibur1987@gmail.com>
2021-09-27 09:09:56 +0300

    checkpoint

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace dotNet_LearnPath
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int fehrenheit = 94;
+
+            decimal celsius = Math.Round(convertFehrenitToCelsius(fehrenheit), 1);
+
+            Console.WriteLine(fehrenheit + " F equals " + celsius + " C.");
+
+            static decimal convertFehrenitToCelsius(decimal fehrenhiteTemp)
+            {
+                return (fehrenhiteTemp - 32) * 5 / 9;
+            }
+        }
+
+
+    }
+}
